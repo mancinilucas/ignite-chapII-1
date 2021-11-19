7a81b0412d9d98df4a110d35d074ddc89115cde1
mancinilucas <mancini.lucasg@gmail.com>
2021-11-18 22:32:00 -0300

    home finalizado

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -23,28 +23,60 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
-
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    const storagedCart = localStorage.getItem('@RocketShoes:cart');
+    //storagedCart recebe os dados do LocalStorage e retorna string ou null
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+      //o método parse converte o valor string de volta ao formato original de array
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const updatedCart = [...cart]; //não usa apenas o valor para manter a imutabilidade
+      const productExists = updatedCart.find(product => product.id === productId);
+
+      const stock = await api.get(`/stock/${productId}`);
+
+      const stockAmount = stock.data.amount;
+      const currentAmount = productExists ? productExists.amount : 0;
+      // Pega o estoque atual e verifica se existe no estoque, senão recebe valor 0
+      const amount = currentAmount + 1;
+
+      if(amount > stockAmount){
+        toast.error('Erro na adição do produto');
+        return;
+        //se a qnt solicitada for maior que o estoque, apresentar msg de erro
+      }
+
+      if(productExists){
+        productExists.amount = amount;
+      } else{
+        const product = await api.get(`/products/${productId}`)
+
+        const newProduct = {
+          ...product.data,
+          amount: 1
+        }
+        updatedCart.push(newProduct);
+        //se o novo produto foi adicionado ao cart, agora a qnt precisa ser alterada.
+      }
+
+      setCart(updatedCart);
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart))
     } catch {
-      // TODO
+      toast.error('Erro na adição do produto');
     }
   };
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
+      
+
     } catch {
-      // TODO
+      
     }
   };
 
